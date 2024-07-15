9bfff7201d9c1a3f7adbbd873522bdd2878fa265
eioforkansi <eioforkansi@gmail.com>
2024-07-15 15:27:38 +0100

    chages

--- models/base_model.py
+++ models/base_model.py
@@ -14,13 +14,9 @@ class BaseModel():
     def __init__(self):
         """Initializes a new BaseModel instance."""
         self.id = str(uuid.uuid4())
+
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
-
-    def __str__(self):
-        """Returns a string representation of the BaseModel instance."""
-        class_name = self.__class__.__name__
-        return "[{}] ({} {})".format(class_name, self.id, self.__dict__)
 
     def save(self):
          """Updates the `updated_at` attribute with the current datetime."""
@@ -32,5 +28,12 @@ class BaseModel():
         dictionary["__class__"] = self.__class__.__name__
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.created_at.isoformat()
+
         return dictionary
+
+    def __str__(self):
+        """Returns a string representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
 
--- tests/test_models/test_review.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/pythons3
-"""
-console.py
-
-This module serves as the entry point into console
-"""
-print("Hello, World")
