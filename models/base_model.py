f12f505cd6383c1a6e4b421f1476bbdccd1e35fc
eioforkansi <eioforkansi@gmail.com>
2024-07-15 07:23:25 +0100

    Let's see

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,36 @@
-#!/usr/bin/pythons3
+#!/usr/bin/python3
 """
-console.py
+base_model.py
 
 This module serves as the entry point into console
 """
-print("Hello, World")
+import uuid
+from datetime import datetime
+
+class BaseModel():
+    """
+
+    """
+    def __init__(self):
+        """Initializes a new BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+
+    def __str__(self):
+        """Returns a string representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({} {})".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+         """Updates the `updated_at` attribute with the current datetime."""
+         self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of the instance."""
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.created_at.isoformat()
+        return dictionary
+
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,14 @@
 #!/usr/bin/pythons3
 """
-console.py
+test_base_model.py
 
 This module serves as the entry point into console
 """
+import os
 import unittest
+from models.base_model import add
 
-print("Hello, World")
+class BaseTest(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(4, 6), 10)
