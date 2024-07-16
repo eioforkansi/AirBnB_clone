66a4901d2e0620fbc56c8b04c1df4011e1d440d6
eioforkansi <eioforkansi@gmail.com>
2024-07-16 18:40:03 +0100

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,15 +29,10 @@ class FileStorage():
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """
-        Serializes __objects to the JSON file (path: __file_path)
-        """
-        obj_dict = {}
-        for key, obj in FileStorage.__objects.items():
-            obj_dict[key] = obj.to_dict()
-
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        obj_dict = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
         with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
-                json.dump(obj_dict, file)
+            json.dump(obj_dict, file)
 
     def reload(self):
         try:
