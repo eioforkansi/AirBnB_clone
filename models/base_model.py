437512b63cb1450f77ff68a2373690774631564c
eioforkansi <eioforkansi@gmail.com>
2024-07-16 09:06:10 +0100

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,21 @@ class BaseModel():
     """
 
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes a new BaseModel instance."""
-        self.id = str(uuid.uuid4())
-
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    new_value = datetime.fromisoformat(value)
+                    setattr(self, key, new_value)
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def save(self):
          """Updates the `updated_at` attribute with the current datetime."""
@@ -27,7 +36,7 @@ class BaseModel():
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
         dictionary["created_at"] = self.created_at.isoformat()
-        dictionary["updated_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
 
         return dictionary
 
